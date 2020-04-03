368693122cca9322587710712a348c60c98eed1a
nbatroff <nbatroff@gmail.com>
2020-04-02 21:09:14 -0700

    Garbage tier stuff

---
+++ EU4 Save Scum/EU4 Save Scum/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EU4_Save_Scum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+            label1.Text = (Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Paradox Interactive\\Europa Universalis IV\\save games");
+        }
+    }
+}
---
+++ EU4 Save Scum/EU4 Save Scum/SaveHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace EU4_Save_Scum
+{
+    class SaveHandler
+    {
+        private string saveDir = (Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Paradox Interactive\\Europa Universalis IV\\save games");
+        public string selectedSave;
+        public string saveHash;
+        public string[] saveFiles;
+        public string[] getSaves()
+        {
+            string[] saveFiles = Directory.GetFiles(saveDir, "*.eu4");
+            return saveFiles;
+        }
+        public void hashSaves()
+        {
+         
+        }
+    }
+}
