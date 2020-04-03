6066b6961e7451d82a91daba426e73f7fe694480
nbatroff <nbatroff@gmail.com>
2020-04-03 07:41:09 -0700

    Checked for Backup files and removed them from the string array.

---
+++ EU4 Save Scum/EU4 Save Scum/ArchiveHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EU4_Save_Scum
+{
+    class ArchiveHandler
+    {
+        public void RenameFile(string path)
+        {
+            string tpath = "C:\\Users\\nbatroff\\Documents\\Paradox Interactive\\Europa Universalis IV\\save games\\Aztec.eu4";
+            var date = DateTime.Now;
+            string newPath = tpath + "suckit"; // date.ToString();
+            File.Copy(tpath, newPath);
+        }
+    }
+}
--- EU4 Save Scum/EU4 Save Scum/Form1.cs
+++ EU4 Save Scum/EU4 Save Scum/Form1.cs
@@ -12,11 +12,39 @@ namespace EU4_Save_Scum
 {
     public partial class Form1 : Form
     {
+        public string[] backupSaves;
+        public string[] savesNoNulls;
         public Form1()
         {
+            SaveHandler newHandler = new SaveHandler();
+            newHandler.getSaves();
             InitializeComponent();
-           
-            label1.Text = (Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Paradox Interactive\\Europa Universalis IV\\save games");
+            string[] fileNames = newHandler.getSaves();
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                // Need to check if value is null. Length will be longer since Backup files are removed.
+                if (fileNames[i] == null)
+                {
+                    break;
+                }
+                else
+                {
+                    savesCheckBox.Items.Add(fileNames[i]);
+                    saveBox.Items.Add(fileNames[i]);
+                }
+            } 
+            label2.Text = newHandler.fileName;
+            label1.Text = (Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Paradox Interactive\\Europa Universalis IV\\save games" + newHandler.selectedSave);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* SaveHandler hashHandler = new SaveHandler();
+            hashHandler.selectedSave = hashHandler.saveDir + "\\tigerZ.eu4";
+            string save = hashHandler.saveDir + "\\tigerZ.eu4";
+            label2.Text = hashHandler.firstHash(save); */
+            ArchiveHandler aHandlder = new ArchiveHandler();
+            aHandlder.RenameFile("test");
         }
     }
 }
